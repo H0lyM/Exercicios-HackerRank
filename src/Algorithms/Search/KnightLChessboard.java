d43f23d24a525d03f7eeba0f5f53ffd9f6bc97a8
Murilo Francisco Bento <murilofranciscobento@gmail.com>
2021-01-06 18:57:13 -0300

    Add files via upload

---
+++ src/Algorithms/Search/KnightLChessboard.java
@@ -0,0 +1,14 @@
+package Algorithms.Search;
+
+public class KnightLChessboard {
+    //https://www.hackerrank.com/challenges/knightl-on-chessboard/problem?h_r=next-challenge&h_v=zen
+
+    static int[][] knightlOnAChessboard(int n) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
