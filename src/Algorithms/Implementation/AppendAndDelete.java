592368a5ae5b12d26fc03454dd23189cb6b8064b
Murilo Francisco Bento <murilofranciscobento@gmail.com>
2021-01-07 19:02:36 -0300

    Add files via upload

---
+++ src/Algorithms/Implementation/AppendAndDelete.java
@@ -0,0 +1,13 @@
+package Algorithms.Implementation;
+
+public class AppendAndDelete {
+    //https://www.hackerrank.com/challenges/append-and-delete/problem?h_r=next-challenge&h_v=zen
+    static String appendAndDelete(String s, String t, int k) {
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
